c3f6c25ea996b610bce02def28bc35b557d8d8cc
Malik Adnan <malikkingoo68@gmail.com>
2022-12-20 20:23:45 +0500

    preparing build version

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
-import Event from "../components/events/Event"
-import {getFeaturedEvents} from "../mock/data"
+import Link from "next/link";
+import { Button } from "reactstrap";
 
 export default function Home() {
-  const events= getFeaturedEvents()
   return (
-   <div>
-    <h1 className="display-1 text-center fw-bold mt-3 text-primary">
-      Featured Events
-    </h1>
-    {events.map(event=>(
-      <Event
-      key={event.id}
-      {...event}
-      />
-    ))}
-
-
-   </div>
-  )
+    <div>
+      <h1 className="display-1 text-center fw-bold mt-3 text-primary">
+        Next Js Basics
+      </h1>
+      <Button color="primary" tag={Link} href='/posts' className="my-5 w-25 mx-auto">
+        View Posts
+      </Button>
+    </div>
+  );
 }
